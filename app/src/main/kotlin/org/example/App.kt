10a6e5954cd13f8c2f18336d6cd771adbcb91422
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2024-11-02 06:49:51 -0500

    Add fizzBuzz anonymous function

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -3,21 +3,24 @@ package org.example
 class App {
     fun run(inputString: String): String {
         val number = inputString.toInt()
-        var returnString = ""
-        if (0 == number % 3) {
-            returnString += "Fizz"
-        }
-        if (0 == number % 5) {
-            returnString += "Buzz"
-        }
-        if (returnString == "") {
-            return number.toString()
-        }
-        return returnString
+        return fizzBuzz(number)
     }
 }
 
+fun fizzBuzz(number: Int): String {
+    var returnString = ""
+    if (0 == number % 3) {
+        returnString += "Fizz"
+    }
+    if (0 == number % 5) {
+        returnString += "Buzz"
+    }
+    if (returnString == "") {
+        return number.toString()
+    }
+    return returnString
 
+}
 
 fun main() {
     println("Main invoked.")
